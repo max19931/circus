698c6163b58bbec571cccdb50f98e5492d4dbc06
Dane Knecht <dok2001@columbia.edu>
2013-11-02 19:05:10 -0700

    make logging a little cleaner.

--- circus/util.py
+++ circus/util.py
@@ -75,7 +75,7 @@ LOG_LEVELS = {
     "info": logging.INFO,
     "debug": logging.DEBUG}
 
-LOG_FMT = r"%(asctime)s [%(process)d] [%(levelname)s] %(message)s"
+LOG_FMT = r"[%(process)d] [%(levelname)s] %(message)s"
 LOG_DATE_FMT = r"%Y-%m-%d %H:%M:%S"
 _SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
 _all_signals = {}
